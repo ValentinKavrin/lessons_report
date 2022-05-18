5a1d4f508a4e9eb7c2331bc2bfe247af413c8c18
ValentinKavrin <kavrin@mail.ru>
2022-05-18 18:50:52 +0300

    Added route for swagger

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+
 const db = require('./models')
 const router = require("./routes/index")
-
+const apiDocs = require("./swagger/index");
 
 const app = express(); 
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json())
-db.sequelize.sync()
+//db.sequelize.sync()
+app.use("/api-docs", apiDocs);
 app.use('/api', router)
 
 
